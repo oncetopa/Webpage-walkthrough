448c285807cc5b77b1fd4a5d2ca13ada2dc11d5b
oncetopa <92215717+oncetopa@users.noreply.github.com>
2021-12-05 15:41:59 +0900

    Update colors.js

--- colors.js
+++ colors.js
@@ -1,8 +1,8 @@
-var Links = {
+      var Links = {
         setColor:function(color){
         $('a').css('color', color);
+        }
       }
-    }
       
       var Body = {
         setColor:function(color){
@@ -13,16 +13,29 @@ var Links = {
         }
       }
       
+      var Button = {
+        setFontColor:function(color){
+        $('input').css('font-color', color);
+        },
+        setBackgroundColor:function(color){
+        $('input').css('background-color', color);
+        }
+      } 
+              
       function nightDayHandler(self){
       if (self.value === 'night'){
         Body.setBackgroundColor('black');
         Body.setColor('white');
+        Button.setFontColor('black');
+        Button.setBackgroundColor('white');
         self.value='day';
 
         Links.setColor('yellow');
         } else {
         Body.setBackgroundColor('white');
         Body.setColor('black');
+        Button.setFontColor('white');
+        Button.setBackgroundColor('black');
         self.value='night';
 
         Links.setColor('#8B00FF');
